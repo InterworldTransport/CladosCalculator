b0771a208da76bbaa68d461623e9480993d4cfb5
adiffer <adiffer@gmail.com>
2020-11-07 22:46:52 -0800

    learning how to do things
    This class will vanish shortly. it's just for learning.
    The real code has to go in the initializer for the calculator and the file open event.

--- src/com/interworldtransport/cladosviewer/NyadListXMLParser.java
+++ src/com/interworldtransport/cladosviewer/NyadListXMLParser.java
@@ -1,45 +1,63 @@
 package com.interworldtransport.cladosviewer;
 
 import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
+import java.io.IOException;
+
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class NyadListXMLParser 
 {
     public static void main(String[] args) throws Exception 
     {
  
-    	final String xmlFilePath = "Snapshot.txt";
+    	final String xmlFilePath = "Snapshot.xml";
         
         //Use method to convert XML string content to XML Document object
-        Document doc = convertXMLFileToXMLDocument( xmlFilePath );
+        Document doc = loadXMLObjectsToXMLDocument( xmlFilePath );
          
         //Verify XML document is build correctly
-        System.out.println(doc.getFirstChild().getNodeName());
+        System.out.println(doc.getBaseURI());
+        System.out.println("NyadLists= "+doc.getChildNodes().getLength());
+        System.out.println("Nyad Count= "+doc.getFirstChild().getAttributes().getNamedItem("size").getTextContent());
+        
+        for (int m = 0; m<doc.getFirstChild().getChildNodes().getLength(); m++)
+        {
+        	System.out.print(doc.getFirstChild().getChildNodes().item(m).getNodeName());
+        	System.out.print("\t");
+        	System.out.println(doc.getFirstChild().getChildNodes().item(m).getChildNodes().getLength());
+        }
+        
         
     }
     
-    private static Document convertXMLFileToXMLDocument(String filePath) 
+    private static Document loadXMLObjectsToXMLDocument(String filePath) 
     {
         //Parser that produces DOM object trees from XML content
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-         
-        //API to obtain DOM Document instance
-        DocumentBuilder builder = null;
-        try
-        {
-            //Create DocumentBuilder with default configuration
-            builder = factory.newDocumentBuilder();
+        factory.setNamespaceAware(true);
+      
+        //Create DocumentBuilder with default configuration
+        try {
+			return factory.newDocumentBuilder().parse(new File(filePath));
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
              
-            //Parse the content to Document object
-            Document doc = builder.parse(new File(filePath));
-            return doc;
-        } 
-        catch (Exception e) 
-        {
-            e.printStackTrace();
-        }
         return null;
+           
+            
+        
+
+
     }
 }    
